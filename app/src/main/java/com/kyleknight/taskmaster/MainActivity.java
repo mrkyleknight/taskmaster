9c15deae80d2fb208bf163838561927e223b11ae
Kyle Knight <knightkyle@outlook.com>
2023-08-17 04:14:04 -0700

    upload lab 26

--- app/src/main/java/com/kyleknight/taskmaster/MainActivity.java
+++ app/src/main/java/com/kyleknight/taskmaster/MainActivity.java
@@ -1,8 +1,10 @@
 package com.kyleknight.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addTaskButton = findViewById(R.id.add_task_button);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTasks.class);
+                startActivity(intent);
+            }
+        });
+
+        Button allTaskButton = findViewById(R.id.all_tasks_button);
+        allTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllTasks.class);
+                startActivity(intent);
+            }
+        });
     }
-} 
+}
