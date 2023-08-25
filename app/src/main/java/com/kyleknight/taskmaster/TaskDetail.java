8bf4543bab9e76a2ed81138a7e5ae4ea4b592392
Kyle Knight <knightkyle@outlook.com>
2023-08-24 23:53:08 -0700

    Upload Lab 29

--- app/src/main/java/com/kyleknight/taskmaster/TaskDetail.java
+++ app/src/main/java/com/kyleknight/taskmaster/TaskDetail.java
@@ -2,8 +2,8 @@ package com.kyleknight.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
-
 public class TaskDetail extends AppCompatActivity {
 
     @Override
@@ -11,13 +11,18 @@ public class TaskDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         String taskTitle = getIntent().getStringExtra("TASK_TITLE");
         String taskDescription = getIntent().getStringExtra("TASK_DESCRIPTION");
-
+        String taskStatus = getIntent().getStringExtra("TASK_STATUS");
 
         TextView titleTextView = findViewById(R.id.textView2);
         TextView descriptionTextView = findViewById(R.id.textView3);
+        TextView statusTextView = findViewById(R.id.statusTextView);
 
         titleTextView.setText(taskTitle);
         descriptionTextView.setText(taskDescription);
